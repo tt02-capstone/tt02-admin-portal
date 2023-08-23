2ac3cece94f70d7ecd1edea69403a0e87501119a
zimmmz <ciensim@gmail.com>
2023-08-23 17:05:41 +0800

    basic nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,32 @@
-export default function Navbar() {
+import { Link } from 'react-router-dom';
+import {Tabs, Tab} from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import LogoutIcon from '@mui/icons-material/Logout';
+
+function Navbar() {
+    const navBarStyle ={
+        border: "1px solid #ccc",
+        color : "darkblue"
+    }
+
+    const logout = {
+        position: "absolute",
+        right: 0
+    }
+
     return (
-        <nav></nav>
+        <nav style={navBarStyle}>
+            <Tabs>
+                <div>
+                    <Tab icon={<HomeIcon />} to="/home" component={Link}/>
+                </div>
+                <div style={logout}>
+                    <Tab label="Welcome Back" disabled/>
+                    <Tab icon={<LogoutIcon />} to="/" component={Link}/>
+                </div>
+            </Tabs>
+        </nav>
     )
 }
+
+export default Navbar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,10 @@
+import Navbar from "../components/Navbar"
+
 export default function Home() {
     return (
-        <h1>Home</h1>
+        <>
+            <Navbar />
+            <h1>Home</h1>
+        </>
     )
 }
