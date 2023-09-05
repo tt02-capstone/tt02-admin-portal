d8c4a4f401ee445d43a4ade390203dd4f8618dda
darryl <darrylgoh51@gmail.com>
2023-09-05 21:40:41 +0800

    changed navbar to app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import CreateAdmin from "./pages/CreateAdmin";
@@ -6,11 +6,35 @@ import 'react-toastify/dist/ReactToastify.css';
 import Navbar from "./components/Navbar";
 import { Layout } from "antd";
 import { useEffect, useState } from "react";
+import { MailOutlined } from '@ant-design/icons';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 function App() {
 
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const [currentTab, setCurrentTab] = useState('/home');
+
+  const menuItems = [
+      {key: '/home', label: 'Home', icon: <MailOutlined />},
+      {key: '/createAdmin', label: 'Create Admin', icon: <MailOutlined />},
+      {key: '/', label: 'Logout',icon: <LogoutIcon />,}
+  ];
+
+  const onClickNewTab = (tab) => {
+      console.log(tab.key);
+      setCurrentTab(tab.key);
+      navigate(tab.key);
+  };
+
   return (
     <div>
+      {location.pathname !== '/' && <Navbar 
+        currentTab={currentTab}
+        menuItems={menuItems}
+        onClickNewTab={onClickNewTab}
+      />}
       <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/home" element={<Home />} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,29 +4,29 @@ import LogoutIcon from '@mui/icons-material/Logout';
 import { Menu } from "antd";
 import { MailOutlined } from '@ant-design/icons';
 
-function Navbar() {
+function Navbar(props) {
     const navigate = useNavigate();
 
-    const [currentTab, setCurrentTab] = useState('/home');
+    // const [currentTab, setCurrentTab] = useState('/home');
 
-    const menuItems = [
-        {key: '/home', label: 'Home', icon: <MailOutlined />},
-        {key: '/createAdmin', label: 'Create Admin', icon: <MailOutlined />},
-        {key: '/', label: 'Logout',icon: <LogoutIcon />,}
-    ];
+    // const menuItems = [
+    //     {key: '/home', label: 'Home', icon: <MailOutlined />},
+    //     {key: '/createAdmin', label: 'Create Admin', icon: <MailOutlined />},
+    //     {key: '/', label: 'Logout',icon: <LogoutIcon />,}
+    // ];
   
-    const onClickNewTab = (tab) => {
-        console.log(tab.key);
-        setCurrentTab(tab.key);
-        navigate(tab.key);
-    };
+    // const onClickNewTab = (tab) => {
+    //     console.log(tab.key);
+    //     setCurrentTab(tab.key);
+    //     navigate(tab.key);
+    // };
 
     return (
         <div style={{display: 'flex', justifyContent: 'space-between'}}>
             <Menu
-                selectedKeys={[currentTab]} 
-                items={menuItems}
-                onClick={onClickNewTab}
+                selectedKeys={[props.currentTab]} 
+                items={props.menuItems}
+                onClick={props.onClickNewTab}
                 style={{display: 'flex', flexDirection: 'column', width: '100%'}}
             />
 
