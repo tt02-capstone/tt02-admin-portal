671e82b9032c389267b48de3de4d44b44dc8d69a
Gabriel Lee <gabriel.lee@u.nus.edu>
2023-09-13 12:56:09 +0800

    Approve or reject new vendor applications

--- src/redux/adminRedux.js
+++ src/redux/adminRedux.js
@@ -3,11 +3,11 @@ import axios from "axios";
 const staffURL = "http://localhost:8080/staff";
 
 export async function createAdmin(admin) {
-    console.log(admin);
-    await axios.post(`${staffURL}/createStaff`, admin)
+  console.log(admin);
+  await axios.post(`${staffURL}/createStaff`, admin)
     .then((response) => {
       if (response.data.httpStatusCode === 400) { // error
-          console.log('fail')
+        console.log('fail')
         return false;
       } else { // success
         console.log("success in adminRedux");
@@ -16,5 +16,30 @@ export async function createAdmin(admin) {
     })
     .catch((error) => {
       console.error("AdminRedux createAdmin Error : ", error);
+    });
+}
+
+export async function getPendingApplications() {
+  try {
+    const response = await axios.get(`${staffURL}/getPendingApplications`);
+    if (response.data != []) {
+      return response.data;
+    }
+  } catch (error) {
+    console.error("AdminRedux getPendingApplications Error : ", error);
+  }
+}
+
+export async function updateApplicationStatus(vendorId, applicationStatus) {
+  await axios.put(`${staffURL}/updateApplicationStatus/${vendorId}/${applicationStatus}`)
+    .then((response) => {
+      if (response.data.httpStatusCode === 400 || response.data.httpStatusCode === 404) {
+        return response.data.errorMessage
+      } else { 
+        return response.data;
+      }
+    })
+    .catch((error) => {
+      console.error("AdminRedux updateApplicationStatus Error : ", error);
     });
 }
