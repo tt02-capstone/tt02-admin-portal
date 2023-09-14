9eebb552b98fae98a39380b8af2dc3df0ba75f22
Gabriel Lee <gabriel.lee@u.nus.edu>
2023-09-14 17:11:35 +0800

    Add sort and filter for table

--- src/pages/PendingApplications.js
+++ src/pages/PendingApplications.js
@@ -1,14 +1,13 @@
 import { Layout } from 'antd';
-import { React, useEffect, useState } from 'react';
+import { React, useEffect, useState, useRef } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import CustomHeader from "../components/CustomHeader";
 import { Content } from "antd/es/layout/layout";
 import { Navigate } from 'react-router-dom';
 import { getPendingApplications, updateApplicationStatus } from '../redux/adminRedux';
-import { Table } from 'antd';
-import {
-    Button
-} from 'antd';
+import { Table, Input, Button, Space } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import Highlighter from 'react-highlight-words';
 
 export default function PendingApplications() {
     const user = JSON.parse(localStorage.getItem("user"));
@@ -63,42 +62,161 @@ export default function PendingApplications() {
         }, 1500);
     }
 
+    const [searchText, setSearchText] = useState('');
+    const [searchedColumn, setSearchedColumn] = useState('');
+    const searchInput = useRef(null);
+    const handleSearch = (selectedKeys, confirm, dataIndex) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+    };
+    const handleReset = (clearFilters) => {
+        clearFilters();
+        setSearchText('');
+    };
+    const getColumnSearchProps = (dataIndex) => ({
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+            <div
+                style={{
+                    padding: 8,
+                }}
+                onKeyDown={(e) => e.stopPropagation()}
+            >
+                <Input
+                    ref={searchInput}
+                    placeholder={`Search ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                    style={{
+                        marginBottom: 8,
+                        display: 'block',
+                    }}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                        icon={<SearchOutlined />}
+                        size="small"
+                        style={{
+                            width: 90,
+                        }}
+                    >
+                        Search
+                    </Button>
+                    <Button
+                        onClick={() => clearFilters && handleReset(clearFilters)}
+                        size="small"
+                        style={{
+                            width: 90,
+                        }}
+                    >
+                        Reset
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            confirm({
+                                closeDropdown: false,
+                            });
+                            setSearchText(selectedKeys[0]);
+                            setSearchedColumn(dataIndex);
+                        }}
+                    >
+                        Filter
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            close();
+                        }}
+                    >
+                        Close
+                    </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: (filtered) => (
+            <SearchOutlined
+                style={{
+                    color: filtered ? '#1677ff' : undefined,
+                }}
+            />
+        ),
+        onFilter: (value, record) =>
+            record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownOpenChange: (visible) => {
+            if (visible) {
+                setTimeout(() => searchInput.current?.select(), 100);
+            }
+        },
+        render: (text) =>
+            searchedColumn === dataIndex ? (
+                <Highlighter
+                    highlightStyle={{
+                        backgroundColor: '#ffc069',
+                        padding: 0,
+                    }}
+                    searchWords={[searchText]}
+                    autoEscape
+                    textToHighlight={text ? text.toString() : ''}
+                />
+            ) : (
+                text
+            ),
+    });
+
     const columns = [
         {
             title: 'Business Name',
             dataIndex: 'business_name',
             key: 'business_name',
-            width: 160
+            width: 160,
+            sorter: (a, b) => a.business_name.localeCompare(b.business_name),
+            ...getColumnSearchProps('business_name'),
         },
         {
             title: 'Vendor Type',
             dataIndex: 'vendor_type',
             key: 'vendor_type',
-            width: 160
+            width: 160,
+            sorter: (a, b) => a.vendor_type.localeCompare(b.vendor_type),
+            ...getColumnSearchProps('vendor_type'),
         },
         {
             title: 'Service Description',
             dataIndex: 'service_description',
             key: 'service_description',
-            width: 240
+            width: 240,
+            sorter: (a, b) => a.service_description.localeCompare(b.service_description),
+            ...getColumnSearchProps('service_description'),
         },
         {
             title: 'POC Name',
             dataIndex: 'poc_name',
             key: 'poc_name',
-            width: 160
+            width: 160,
+            sorter: (a, b) => a.poc_name.localeCompare(b.poc_name),
+            ...getColumnSearchProps('poc_name'),
         },
         {
             title: 'POC Position',
             dataIndex: 'poc_position',
             key: 'poc_position',
-            width: 160
+            width: 160,
+            sorter: (a, b) => a.poc_position.localeCompare(b.poc_position),
+            ...getColumnSearchProps('poc_position'),
         },
         {
             title: 'POC Contact',
             dataIndex: 'poc_contact',
             key: 'poc_contact',
-            width: 160
+            width: 160,
+            sorter: (a, b) => a.poc_contact.localeCompare(b.poc_contact),
+            ...getColumnSearchProps('poc_contact'),
         },
         {
             title: 'Approve / Reject',
