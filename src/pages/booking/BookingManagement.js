c9f8e22cec1e3b1c0ed8990185d30dc71a0e1ca8
kellyco540 <kellyco540@gmail.com>
2023-10-01 10:37:10 +0800

    done filtering of bookings

--- src/pages/booking/BookingManagement.js
+++ src/pages/booking/BookingManagement.js
@@ -12,6 +12,7 @@ import CustomTablePagination from "../../components/CustomTablePagination";
 import { SearchOutlined } from '@ant-design/icons';
 import Highlighter from 'react-highlight-words';
 import { ToastContainer, toast } from 'react-toastify';
+import moment from 'moment';
 
 export default function BookingManagement() {
 
@@ -28,35 +29,188 @@ export default function BookingManagement() {
         }
     ];
 
+    const [searchText, setSearchText] = useState('');
+    const [searchedColumn, setSearchedColumn] = useState('');
+    const searchInput = useRef(null);
+    const handleSearch = (selectedKeys, confirm, dataIndex) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+    };
+    const handleReset = (clearFilters) => {
+        clearFilters();
+        setSearchText('');
+    };
+    const getColumnSearchProps = (dataIndex) => ({
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+            <div
+                style={{
+                    padding: 8,
+                }}
+                onKeyDown={(e) => e.stopPropagation()}
+            >
+                <Input
+                    ref={searchInput}
+                    placeholder={`Search ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                    style={{
+                        marginBottom: 8,
+                        display: 'block',
+                    }}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                        icon={<SearchOutlined />}
+                        size="small"
+                        style={{
+                            width: 90,
+                        }}
+                    >
+                        Search
+                    </Button>
+                    <Button
+                        onClick={() => clearFilters && handleReset(clearFilters)}
+                        size="small"
+                        style={{
+                            width: 90,
+                        }}
+                    >
+                        Reset
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            close();
+                        }}
+                    >
+                        Close
+                    </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: (filtered) => (
+            <SearchOutlined
+                style={{
+                    color: filtered ? '#1677ff' : undefined,
+                }}
+            />
+        ),
+        onFilter: (value, record) =>
+            record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownOpenChange: (visible) => {
+            if (visible) {
+                setTimeout(() => searchInput.current?.select(), 100);
+            }
+        },
+        render: (text) =>
+            searchedColumn === dataIndex ? (
+                <Highlighter
+                    highlightStyle={{
+                        backgroundColor: '#ffc069',
+                        padding: 0,
+                    }}
+                    searchWords={[searchText]}
+                    autoEscape
+                    textToHighlight={text ? text.toString() : ''}
+                />
+            ) : (
+                text
+            ),
+    });
+
     const bookingsColumns = [
         {
+            title: 'Id',
+            dataIndex: 'booking_id',
+            key: 'booking_id',
+            sorter: (a, b) => a.booking_id > b.booking_id,
+            ...getColumnSearchProps('booking_id'),
+        },
+        {
             title: 'Customer Name',
-            dataIndex: 'customerName',
-            key: 'customerName',
-            render: (text, record) => {
-                const customerName = record.tourist_user
-                    ? record.tourist_user.name
-                    : record.local_user
-                    ? record.local_user.name
-                    : '';
-        
-                return customerName;
-            },
+            dataIndex: 'name',
+            key: 'name',
+            sorter: (a, b) => a.name > b.name,
+            ...getColumnSearchProps('name'),
         },
         {
             title: 'Customer Type',
-            dataIndex: 'customerType',
-            key: 'customerType',
+            dataIndex: 'booked_user',
+            key: 'booked_user',
+            filters: [
+                {
+                    text: 'Local',
+                    value: 'LOCAL',
+                },
+                {
+                    text: 'Tourist',
+                    value: 'TOURIST',
+                },
+            ],
+            onFilter: (value, record) => record.booked_user === value,
             render: (text, record) => {
-              if (record.tourist_user) {
-                return 'Tourist';
-              } else if (record.local_user) {
-                return 'Local';
+              if (text === 'LOCAL') {
+                return <Tag color='success'>Local</Tag>;
+              } else if (text === 'TOURIST') {
+                return <Tag color='error'>Tourist</Tag>;
               } else {
-                return '';
+                return 'Bug';
               }
             },
-          },
+        },
+        {
+            title: 'Type',
+            dataIndex: 'type',
+            key: 'type',
+            filters: [
+                {
+                    text: 'Accommodation',
+                    value: 'ACCOMMODATION',
+                },
+                {
+                    text: 'Telecom',
+                    value: 'TELECOM',
+                },
+                {
+                    text: 'Attraction',
+                    value: 'ATTRACTION',
+                },
+                {
+                    text: 'Tour',
+                    value: 'TOUR',
+                },
+            ],
+            onFilter: (value, record) => record.type === value,
+            render: (text, record) => {
+                let color = '';
+                let value = '';
+                switch (text) {
+                    case 'ACCOMMODATION':
+                        color = 'processing';
+                        value = 'Accommodation';
+                        break;
+                    case 'TELECOM':
+                        color = 'warning';
+                        value = 'Telecom';
+                        break;
+                    case 'ATTRACTION':
+                        color = 'success';
+                        value = 'Attraction';
+                        break;
+                    case 'TOUR':
+                        color = 'error';
+                        value = 'Tour';
+                        break;
+                }
+
+                return <Tag color={color}>{value}</Tag>;
+            },
+        },
         {
             title: 'Activity',
             dataIndex: 'activity_name',
@@ -66,39 +220,34 @@ export default function BookingManagement() {
                 console.log("record", record);
                 return record.activity_name;
             },
-        },
-        {
-            title: 'Type',
-            dataIndex: 'type',
-            key: 'type',
-            onFilter: (value, record) => record.type === value,
-            render: (type) => {
-                let color = '';
-                switch (type) {
-                    case 'ACCOMMODATION':
-                        color = 'green';
-                        break;
-                    case 'ATTRACTION':
-                        color = 'orange';
-                        break;
-                    case 'TELECOM':
-                        color = 'red';
-                        break;
-                    case 'TOUR':
-                        color = 'blue';
-                        break;
-                }
-
-                return <Tag color={color}>{type}</Tag>;
-            },
+            sorter: (a, b) => a.activity_name > b.activity_name,
+            ...getColumnSearchProps('activity_name'),
         },
         {
             title: 'Status',
             dataIndex: 'status',
             key: 'status',
+            filters: [
+                {
+                    text: 'Upcoming',
+                    value: 'UPCOMING',
+                },
+                {
+                    text: 'Ongoing',
+                    value: 'ONGOING',
+                },
+                {
+                    text: 'Completed',
+                    value: 'COMPLETED',
+                },
+                {
+                    text: 'Cancelled',
+                    value: 'CANCELLED',
+                },
+            ],
             onFilter: (value, record) => record.status === value,
             render: (status) => {
-                let color = '';
+                let color = 'default';
                 switch (status) {
                     case 'UPCOMING':
                         color = 'processing';
@@ -121,17 +270,24 @@ export default function BookingManagement() {
             title: 'Last Updated',
             dataIndex: 'last_update',
             key: 'last_update',
-            render: (lastUpdate) => {
-                const dateObj = new Date(lastUpdate);
-                const formattedDate = `${dateObj.getDate().toString().padStart(2, '0')}/${(dateObj.getMonth() + 1).toString().padStart(2, '0')}/${dateObj.getFullYear()} ${dateObj.toLocaleTimeString()}`;
-                return formattedDate;
-            },
+            sorter: (a, b) => new Date(a.last_update) > new Date(b.last_update),
+            ...getColumnSearchProps('last_update'),
         },
         {
             title: 'Payment Status',
             dataIndex: 'payment',
             key: 'payment',
-            onFilter: (value, record) => record.is_paid === value,
+            filters: [
+                {
+                    text: 'Paid',
+                    value: true,
+                },
+                {
+                    text: 'Unpaid',
+                    value: false,
+                },
+            ],
+            onFilter: (value, record) => record.payment.is_paid === value,
             render: (payment) => {
                 let color = '';
                 if (payment && payment.is_paid) {
@@ -140,16 +296,15 @@ export default function BookingManagement() {
                     color = 'red';
                 }
 
-                return <Tag color={color}>{payment ? (payment.is_paid ? 'PAID' : 'UNPAID') : 'N/A'}</Tag>;
+                return <Tag color={color}>{payment ? (payment.is_paid ? 'Paid' : 'Unpaid') : 'N/A'}</Tag>;
             },
         },
         {
             title: 'Amount Earned',
-            dataIndex: 'payment',
-            key: 'payment',
-            render: (payment) => {
-                return `$${(payment.payment_amount * (payment.comission_percentage)).toFixed(2)}`
-            },
+            dataIndex: 'payment_amount',
+            key: 'payment_amount',
+            sorter: (a, b) => a.payment_amount > b.payment_amount,
+            ...getColumnSearchProps('payment_amount'),
         },
         {
             title: 'Action(s)',
@@ -174,6 +329,11 @@ export default function BookingManagement() {
                 if (response.status) {
                     var tempData = response.data.map((val) => ({
                         ...val,
+                        name: val.booked_user === 'LOCAL' ? val.local_user.name : val.tourist_user.name,
+                        last_update: moment(val.last_update).format('llll'),
+                        start_datetime: moment(val.start_datetime).format('ll'),
+                        end_datetime: moment(val.end_datetime).format('ll'),
+                        payment_amount: `$${(val.payment.payment_amount * val.payment.comission_percentage).toFixed(2)}`,
                         key: val.user_id,
                     }));
                     setBookingsData(tempData);
@@ -218,8 +378,8 @@ export default function BookingManagement() {
                             title="Bookings"
                             column={bookingsColumns}
                             data={bookingsData}
+                            rowKey="booking_id"
                             tableLayout="fixed"
-                            
                         />
 
                         <ViewBookingModal
